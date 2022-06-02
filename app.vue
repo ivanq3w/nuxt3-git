197bd81aca5f8d69232084acfa5738e22479d317
ivanq3w <ivanq3w@gmail.com>
2022-06-02 20:15:45 +0300

    Fixed texts for green btn

--- app.vue
+++ app.vue
@@ -3,6 +3,6 @@
     <!-- <NuxtWelcome /> -->
     <router-view></router-view>
     <button>my button</button>
-    <button style="color: white; background: green;">Green Button</button>
+    <button style="color: white; background: green;">Green Button!</button>
   </div>
 </template>
