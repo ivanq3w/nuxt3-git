f1d1597df2928419981979edf5db13f74ca63518
Ivan Que <58051684+ivanq3w@users.noreply.github.com>
2022-06-02 20:28:15 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -5,5 +5,6 @@
     <button>my button</button>
     <button style="color: white; background: green;">Green Button!</button>
     <button style="background: red">Red button</button>
+    <h1>Buttons</h1>
   </div>
 </template>
