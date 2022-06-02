3599d24932d2b808e5c91ca8f899a411a7abd2b8
Ivan Que <58051684+ivanq3w@users.noreply.github.com>
2022-06-02 19:19:00 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <router-view></router-view>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="display: flex; flex-direction: column">
+    <h1>Index</h1>
+    <router-link to="/one">One</router-link>
+    <router-link to="/two">Two</router-link>
+  </div>
+</template>
